bc830e5bfb6aa384101e15c707c5695f4d0f53f9
Ben <ben@familysafeinternet.com>
2025-05-19 00:44:18 -0400

    Update suggestion mapping

--- src/mapper/mapper.js
+++ src/mapper/mapper.js
@@ -26,27 +26,49 @@ function combineArticles(resArr) {
 }
 
 function combineSuggestions(sugArr) {
-    const combMap = new Map();
+    const comb = new Map();
     sugArr.forEach(s => {
         s.forEach(r => {
-            const [q, engines] = r;
-            if (!combMap.has(q)) {
-                combMap.set(q, r);
+            const { q, e, p } = r;
+            if (!comb.has(q)) {
+                comb.set(q, r);
             }
-            const cr = combMap.get(q);
-            engines.forEach(e => {
-                if (cr[1].indexOf(e) >= 0) {
-                    return;
-                }
-                cr[1].push(e);
+
+            const cr = comb.get(q);
+            e.forEach(en => {
+                if (cr.e.includes(en)) return;
+                cr.e.push(en);
             });
         });
     });
-    const combArr = Array.from(combMap.values());
-    combArr.sort((a,b) => {
-        return (a[0].length - b[0].length) + (b[1].length - a[1].length);
+    const sort = Array.from(comb.values());
+    sort.forEach(s => {
+        const places = s.e.map(e => +e.place);
+        
+        // Remove a single outlier if it skews the average significantly
+        let filteredPlaces = [...places];
+        if (places.length >= 4) {
+            const mean = places.reduce((a, b) => a + b, 0) / places.length;
+            const stdev = Math.sqrt(places.map(p => Math.pow(p - mean, 2)).reduce((a, b) => a + b, 0) / places.length);
+        
+            const outliers = places.filter(p => Math.abs(p - mean) > stdev * 1.5);
+            if (outliers.length === 1) {
+                filteredPlaces = places.filter(p => p !== outliers[0]);
+            }
+        }
+        
+        const avgPlace = 1 + filteredPlaces.reduce((o, c) => o + c, 0) / filteredPlaces.length;
+        
+        const appearanceBias = s.e.length < 2 
+            ? -1  // penalize less appearances
+            : (s.e.length - 2) * 0.2;  // slight boost beyond 2
+        
+        const lengthBias = Math.exp(-Math.pow(s.q.length - 15, 2) / (2 * Math.pow(5, 2))) * 0.2;
+        
+        s.p = parseFloat((avgPlace - appearanceBias - lengthBias).toFixed(2));
     });
-    return combArr;
+    sort.sort((a,b) => a.p - b.p);
+    return sort;
 }
 
 module.exports = {
